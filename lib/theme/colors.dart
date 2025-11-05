70e7548d9749fab64845acb743bf8a1676fb3361
gyuwonsong <gyuwon0722@kookmin.ac.kr>
2025-11-06 03:31:48 +0900

    design: 색상 팔레트 지정

---
+++ lib/theme/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary    = Color(0xFF1982EF); // #1982EF
+  static const secondary  = Color(0xFF000000); // #000000
+  static const tertiary   = Color(0xFFE3E3E3); // #E3E3E3
+  static const quaternary = Color(0xFF757171); // #757171
+  static const white      = Color(0xFFFFFFFF); // #FFFFFF
+  static const background = Color(0xFFF7F8FA); // #F7F8FA
+  static const disable    = Color(0xFF8C8C8C); // #8C8C8C
+  static const error      = Color(0xFFB3261E); // #B3261E
+}
